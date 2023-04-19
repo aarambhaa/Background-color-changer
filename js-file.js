8ce4c7fbb308cd755d2206828fe4d39e40b8d4e0
Aarambha Adhikari <79296501+aarambhaa@users.noreply.github.com>
2023-04-19 16:37:10 +0545

    Add files via upload

---
+++ js-file.js
@@ -0,0 +1,28 @@
+function defaultColor() {
+   window.location.reload();
+}
+
+function BlueColor() {
+   document.body.style.backgroundColor = "#0970D9";
+   document.getElementById('h1').style.color = "white";
+}
+
+function yellowColor() {
+   document.body.style.backgroundColor = "yellow";
+   document.getElementById('h1').style.color = "black";
+}
+
+function redColor() { 
+   document.body.style.backgroundColor = "red";
+   document.getElementById('h1').style.color = "white";
+}
+
+function blackColor() { 
+   document.body.style.backgroundColor = "black";
+   document.getElementById('h1').style.color = "white";
+}
+
+function greenColor() {
+   document.body.style.backgroundColor = "#0DF500";
+   document.getElementById('h1').style.color = "black";
+}
